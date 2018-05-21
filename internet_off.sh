9dbc40daac1f63c81dbe9e5756ff109c7b4060b4
Artur Rodrigues <arturlr@users.noreply.github.com>
2018-05-21 12:11:25 -0700

    Delete internet_off.sh

--- internet_off.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-iptables -I forwarding_rule -m comment --comment "shut off computer1" -p all -m mac --mac-source 33:33:33:33:33:33 -j REJECT
-iptables -I forwarding_rule -m comment --comment "shut off device1" -p all -m mac --mac-source 44:44:44:44:44:44 -j REJECT
